176157700b10bd6d60066bbed21f7f3a81048a8d
Tharsan <trp4145@bell.net>
2017-12-07 11:00:27 -0500

    Android

---
+++ ECSE_426_FINAL_PROJECT/BLE_SampleProject/Inc/main.h
@@ -0,0 +1,6 @@
+#define RX_ARR_SIZE   50
+#define TIMEOUT       2000
+
+extern uint8_t audio_data[RX_ARR_SIZE];
+
+void receive_data(void);
---
+++ ECSE_426_FINAL_PROJECT/BLE_Tutorial_Final/app/src/main/java/android/kaviles/bletutorial/Storage.java
@@ -0,0 +1,8 @@
+package android.kaviles.bletutorial;
+
+/**
+ * Created by trp41 on 12/7/2017.
+ */
+
+public class Storage {
+}
